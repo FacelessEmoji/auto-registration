bf9ffe1ac6d686472989698945b271dcee8065c6
Emoji <studyemoji@mail.ru>
2024-10-14 22:49:01 +0300

    added basic db staff

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from project.main import main
 from project.functions import load_proxies, parse_accounts_from_txt
 
 if __name__ == "__main__":
-    accounts_path = "accounts.txt"
+    accounts_path = "db/accounts.txt"
     proxy_path = "proxies.txt"
 
     proxy = load_proxies(proxy_path)
     accounts = parse_accounts_from_txt(accounts_path)
-    main(proxy, accounts, accounts_path)
+    main(proxy, accounts)
